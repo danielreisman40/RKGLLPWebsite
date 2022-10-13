4072c612923ba1cbc6f735ecebf25d8aa6aba883
Daniel <daniel.reisman@student.fairfield.edu>
2022-10-13 15:53:49 -0400

    Updated RKG Page

---
+++ fcf-assets/fcf.process.php
@@ -0,0 +1,12 @@
+<?php
+// ************************************
+// This file is part of a package from:
+// www.freecontactform.com
+// See license for details
+// ************************************
+
+require 'fcf-assets/fcf.config.php';
+require 'fcf-assets/process.php';
+
+//require dirname(__FILE__).'/'.'fcf.config.php';
+//require dirname(__FILE__).'/'.'process.php';
